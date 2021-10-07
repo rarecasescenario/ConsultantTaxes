eb22791f23aef523ae40e881df478e779dfcce27
Rare, Case.Scenario <rare.case.scenario@gmail.com>
2021-10-07 06:06:20 -0400

    inner beans

--- KoushikSpring/src/main/java/org/tutorials/o4/Triangle.java
+++ KoushikSpring/src/main/java/org/tutorials/o4/Triangle.java
@@ -3,21 +3,42 @@ package org.tutorials.o4;
 
 public class Triangle {
 
-	private String message;
-	
-	private String type;
-	
-	
-	public String getType() {
-		return type;
-	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
+	private String message = "Tutorial Spring with xml"; 
+	
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
 	public void draw() {
-		System.out.println("Triangle worked...");
+		System.out.println(message);
+        System.out.println(pointA.getX() + "  " + pointA.getY());
+        System.out.println(pointB.getX() + "  " + pointB.getY());
+        System.out.println(pointC.getX() + "  " + pointC.getY());
+	}
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public String getMessage() {
@@ -28,6 +49,5 @@ public class Triangle {
 		this.message = message;
 	}
 
-	
 	
 }
