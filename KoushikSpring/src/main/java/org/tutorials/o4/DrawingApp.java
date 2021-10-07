75d6016ded06f87a4ab01d16f46eeab53d41fbc7
Rare, Case.Scenario <rare.case.scenario@gmail.com>
2021-10-07 05:33:45 -0400

    ongoing updates

--- KoushikSpring/src/main/java/org/tutorials/o4/DrawingApp.java
+++ KoushikSpring/src/main/java/org/tutorials/o4/DrawingApp.java
@@ -3,6 +3,8 @@ package org.tutorials.o4;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class DrawingApp {
@@ -15,12 +17,27 @@ public class DrawingApp {
 	 */
 	public static void main(String[] args) {
 
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle tr = context.getBean("triangle", Triangle.class);
+		 tr.draw();
+	}
+	
+	
+	
+	
+	
+	
+	/**
+	 * beanDefinitionsTest();
+	 */
+	private static void beanDefinitionsTest() {
 		BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
 		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-		reader.loadBeanDefinitions(new ClassPathResource("Beans.xml"));
+		reader.loadBeanDefinitions(new ClassPathResource("spring.xml"));
 
 		Triangle tr = (Triangle) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("triangle");
 		tr.draw();
+		
 	}
 
 }
--- KoushikSpring/src/main/java/org/tutorials/o4/Triangle.java
+++ KoushikSpring/src/main/java/org/tutorials/o4/Triangle.java
@@ -5,6 +5,17 @@ public class Triangle {
 
 	private String message;
 	
+	private String type;
+	
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public void draw() {
 		System.out.println("Triangle worked...");
 	}
